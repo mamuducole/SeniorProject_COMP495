0d2635c57422b28984f1181030aa302ad63992eb
BryceFerguson04 <183842161+BryceFerguson04@users.noreply.github.com>
2026-05-16 05:25:06 +0000

    Updated the map. Still need to make a few tweaks

--- templatemo_569_edu_meeting/usa-map.js
+++ templatemo_569_edu_meeting/usa-map.js
@@ -1,8 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const states = document.querySelectorAll('.state');
+document.addEventListener("DOMContentLoaded", () => {
+    const mappContainer = document.querySelector("#usa_map");
 
-    //Handle Hover
-    state.addEventListener('mouseenter', () => {
-        console.log("Entering:", state.id);
-    }); 
+    if(!mappContainer) return;
+
+    /* Hover effects (Mouseenter) */
+    mappContainer.addEventListener("mouseover", (event) => {
+        if(event.target.classList.contains("state")) {
+            event.target.classList.add("hover");
+        }
+    });
+
+    /* Hover effects (Mouse leaves) */
+    mappContainer.addEventListener("mouseout", (event) => {
+        if(event.target.classList.contains("state")) {
+            event.target.classList.remove("hover");
+        }
+    });
+
+    /* Click events */
+    mappContainer.addEventListener("click", (event) => {
+        if(event.target.classList.contains("state")) {
+            const currentHighlighted = mappContainer.querySelector(".highlight-stance");
+            if(currentHighlighted && currentHighlighted !== event.target) {
+                currentHighlighted.classList.remove("highlight-stance");
+            }
+
+            event.target.classList.toggle("highlight-stance");
+
+            const stateId = event.target.id;
+            console.log('You clicked on: ${stateId}');
+        }
+
+        const linkTarget = event.target.closest("a");
+        if(linkTarget) {
+            return;
+        }
+
+    });
 });
