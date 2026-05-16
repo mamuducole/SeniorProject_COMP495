bd883b2452a3c48a3db50aaf8433976ad87706c4
BryceFerguson04 <183842161+BryceFerguson04@users.noreply.github.com>
2026-05-16 05:01:11 +0000

    Updated the map. Still needs a few tweaks

---
+++ templatemo_569_edu_meeting/usa-map.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const states = document.querySelectorAll('.state');
+
+    //Handle Hover
+    state.addEventListener('mouseenter', () => {
+        console.log("Entering:", state.id);
+    }); 
+});
